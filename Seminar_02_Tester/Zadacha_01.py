f6b0891497c181cadaffe22c6dd28cab5de00f78
Natalya <ivanechka93@mail.ru>
2022-09-11 17:37:17 +0300

    01

--- Seminar_02_Tester/Zadacha_01.py
+++ Seminar_02_Tester/Zadacha_01.py
@@ -3,3 +3,21 @@
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
+
+
+n = input("Введите число N = ")
+sum = 0
+for digit in n:
+    if digit.isdigit():
+        sum += int(digit)
+print(f"Сумма цифр введенного числа {n} = {sum}")
+
+
+#  ====== для натуральных чисел ==========
+# n = float(input("Введите число N: "))
+# sum = 0
+# while n > 0:
+#     digit = n % 10
+#     sum += digit
+#     n = n // 10
+# print (f"Сумма цифр введенного числа {n} = {sum}")
