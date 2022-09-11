ae639a0f974cc1dec34b5b1dc9d3600959531421
Natalya <ivanechka93@mail.ru>
2022-09-11 20:26:02 +0300

    сумма списка последовательности

--- Seminar_02_Tester/Zadacha_03.py
+++ Seminar_02_Tester/Zadacha_03.py
@@ -1,2 +1,14 @@
 # 3. Задайте список из n чисел последовательности 
 # $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+n = int(input("Введите размер списка: n = "))
+sumElem = 0
+a = []
+
+for i in range(n):
+    new_elem = (1+1/(i+1)**(i+1))
+    a.append(new_elem)
+print(f"Список: {a}")
+
+sumElem = sum(a)
+print(f"Сумма чисел списка = {sumElem}")
