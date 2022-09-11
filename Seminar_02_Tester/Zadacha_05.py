0a2caf8996aceca4bc44d52404c41c7cde214b3b
Natalya <ivanechka93@mail.ru>
2022-09-11 20:37:41 +0300

    перемешивание списка

--- Seminar_02_Tester/Zadacha_05.py
+++ Seminar_02_Tester/Zadacha_05.py
@@ -1,1 +1,18 @@
 # 5. Реализуйте алгоритм перемешивания списка.
+
+import random
+
+n = int(input("Введите размер списка: "))
+a = []
+for i in range(n):
+    newElem = random.randint(0, n) # randint - Возвращает случайное целое число в диапазоне [a, b], включая обе конечные точки.
+    a.append(newElem)
+print(f"Cписок: {a}")
+random.shuffle(a) # shuffle - перемешивает список
+print(f"Перемешанный список: {a}")
+
+
+# import random # генерация случайных чисел
+# list = [1, 2, 3, 4, 5]
+# result = random.sample(list, len(list)) # sample - возвращает новый список, содержащий элементы из совокупности, оставляя исходную совокупность без изменений. 
+# print ("\n", str(list), "-->", str(result))
