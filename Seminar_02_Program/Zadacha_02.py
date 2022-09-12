c1a1254af1a3458285f444381603e690cf13ebdc
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-12 11:12:47 +0300

    набор произведений числа с проверкой на ввод числа

--- Seminar_02_Program/Zadacha_02.py
+++ Seminar_02_Program/Zadacha_02.py
@@ -6,10 +6,23 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-N = int(input("Введите число N = "))
+# N = int(input("Введите число N = "))
+
+
+def input_numbers(input_text): # функция проверки на ввод числа
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{input_text}"))
+            is_OK = True
+        except ValueError:
+                print("Ошибка! Это не число!")
+    return number
+
+N = input_numbers("Введите число: ")
 result = 1
 result_list = []
 for i in range(1, N + 1):
         result *= i
         result_list.append(result) # append - добавляет объект в конец списка
-        print(result_list)
+print(result_list)
