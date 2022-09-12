1824fb331eb3c938128d2cfc7148b8e0198a4d86
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-12 13:32:04 +0300

    выдача случайного числа

--- Seminar_02_Program/Zadacha_04.py
+++ Seminar_02_Program/Zadacha_04.py
@@ -2,3 +2,15 @@
 # не использовать random.randint и вообще библиотеку random
 # Можете использовать xor, биты, библиотеку time или datetime (миллисекунды или наносекунды) - для задания случайности
 # Учтите, что есть диапазон: от(минимальное) и до (максимальное)
+
+
+from datetime import datetime
+import time, math
+
+def randome(min, max):
+    d = max - min # берем промежуток, разницу между мин и макс
+    ms = datetime.today().microsecond / (10 ** 6)
+    print(f"{ms}")
+    return min + math.ceil(d * ms) # math.ceil округление
+
+print(randome(0,10))
