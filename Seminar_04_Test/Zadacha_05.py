3273df60cf3edc06f8a5a950f4b710aa968dd7ba
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-14 16:29:47 +0300

    05

---
+++ Seminar_04_Test/Zadacha_05.py
@@ -0,0 +1,26 @@
+# 5. Даны два файла, в каждом из которых находится запись многочлена. 
+# Задача - сформировать файл, содержащий сумму многочленов.
+
+
+# def polynom():
+#     import random
+#     k = [random.randint(1, 100) for i in range(random.randint(2, 10))]
+#     print('Натуральная степень: n =', len(k)-1) 
+#     m = ''
+#     for i in range(0, len(k)):
+#         m = m + str(k[i]) 
+#         if i < len(k) - 1:
+#             m += 'х'
+#         if i < len(k)- 2:
+#             m += '^'+ str(len(k) - i - 1)
+#         if i < len(k) - 1:
+#             m += ' + '
+#     m += ' = 0'
+#     return (m)
+    
+# p1 = polynom()
+# p2 = polynom()
+# print(p1)
+# print(p2)
+
+def koeff(str):
