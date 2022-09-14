837c9f340c67af227ee908ff328862daee1a1b5c
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-14 16:23:01 +0300

    список простых множителей числа, через факторизацию

---
+++ Seminar_04_Test/Zadacha_02.py
@@ -0,0 +1,19 @@
+# 2. Задайте натуральное число N. 
+# Напишите программу, которая составит список простых множителей числа N.
+
+n = int(input('Задайте натуральное число: '))
+
+def Factorization(n): # функция факторизации перебором делителей
+    divisor = [] # список делителей
+    d = 2 # делитель
+    while d * d <= n:
+        if n % d == 0:
+            divisor.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        divisor.append(n)
+        return divisor
+
+print('\n', n, '->', Factorization(n), '\n')
