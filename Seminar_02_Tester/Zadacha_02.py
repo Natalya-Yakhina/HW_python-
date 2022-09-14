88c4746f8c519bf056165c20cf36faf5c02df516
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-14 11:39:03 +0300

    набор произведений от 1 до н

--- Seminar_02_Tester/Zadacha_02.py
+++ Seminar_02_Tester/Zadacha_02.py
@@ -3,10 +3,17 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-N = int(input("Введите число N = "))
-result = 1
-result_list = []
-for i in range(1, N + 1): # range - возвращает объект, который производит последовательность целых чисел от начала до остановки пошагово.
-    result *= i
-    result_list.append(result) # append - добавляет объект в конец списка
-print(result_list)
+# N = int(input("Введите число N = "))
+# result = 1
+# result_list = []
+# for i in range(1, N + 1): # range - возвращает объект, который производит последовательность целых чисел от начала до остановки пошагово.
+#     result *= i
+#     result_list.append(result) # append - добавляет объект в конец списка
+# print(result_list)
+
+number = input('Введите число: ')
+sum = 0
+for i in number:
+    if i.isdigit():
+        sum += int(i)
+print(int(sum))
