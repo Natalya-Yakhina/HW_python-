77c8e08c1ea5fa2e9a245969443a3ea4456c95dc
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-19 10:22:18 +0300

    изменение фамилии в файле/семинар

--- Seminar_04_Program/Zadacha_03.py
+++ Seminar_04_Program/Zadacha_03.py
@@ -1,4 +1,5 @@
 # 3 - В файле, содержащем фамилии студентов и их оценки, изменить на прописные буквы фамилии тех студентов, которые имеют средний балл более «4».
+
 # Нужно перезаписать файл.
 # Пример:
 # Ангела Меркель 5
@@ -11,3 +12,15 @@
 # АНДРЕЙ ВАЛЕТОВ 5
 # Фредди Меркури 3
 # Анастасия Пономарева 4
+
+list_student = []
+with open('student.txt', 'w', encoding='utf-8') as file_obg:
+    for line in file_obg:
+        if '5' in line:
+            line = line.upper() # Возвращает копию строки, преобразованную в верхний регистр
+        list_student.append(line.replace('\n', ''))
+print(list_student)
+
+with open('main.txt', 'w', encoding='utf-8') as file_obg:
+    for line in list_student:
+        file_obg.write(line + '\n')
