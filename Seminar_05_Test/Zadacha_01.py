f04515206220be060c9669d92625935e1e1fc846
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-19 16:08:33 +0300

    удаляет из текста слова

--- Seminar_05_Test/Zadacha_01.py
+++ Seminar_05_Test/Zadacha_01.py
@@ -10,8 +10,16 @@ text = delete(text)
 print(text)
 
 # ===================================================================================
-
 # newTxt = 'Напишите абв програбву программу удаляющуабв удаляющую из текста все слова словабв, содержащие абв'.split()
 # my_str = 'абв'
 # result = list(filter(lambda x: not my_str in x, newTxt))
 # print(result)
+# ===================================================================================
+# with open("words.txt", "r") as fin:
+#     for line in fin:
+#         words = line.split()
+#     for word in words:
+#         if "абв" in word:
+#             words.remove(word)
+# sentence = " ".join(words)
+# print(sentence)
