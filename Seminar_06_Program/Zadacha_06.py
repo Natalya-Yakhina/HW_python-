1cab45470351a7e010f2f968e06f7d77ca757637
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-26 11:11:25 +0300

    список последовательности

--- Seminar_06_Program/Zadacha_06.py
+++ Seminar_06_Program/Zadacha_06.py
@@ -2,9 +2,13 @@
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
 
+# N = int(input("Введите число: "))
+# print(f"Для N = {N} -> ", end = "")
+# for i in range(0, N-1):
+#     print((-3)**i, end= ", ")
+# else:
+#     print((-3)**(N-1))
+
+
 N = int(input("Введите число: "))
-print(f"Для N = {N} -> ", end = "")
-for i in range(0, N-1):
-    print((-3)**i, end= ", ")
-else:
-    print((-3)**(N-1))
+print(list(map(lambda x: (-3)**x, range(N))))
