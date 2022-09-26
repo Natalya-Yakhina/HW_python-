3422410fed518e13dce0ad7f06c853f48f4ebf8a
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-26 10:36:23 +0300

    присутствует ли число в списке

--- Seminar_06_Program/Zadacha_01.py
+++ Seminar_06_Program/Zadacha_01.py
@@ -2,6 +2,8 @@
 # присутствует ли в заданном списке строк некое число.
 # ['апап4', 'fdgg3', 'fgdf', '6', 'fg24'] - ищем 24 - найдено на 4 индексе
 
+
+# ===================================  1 ===========================================
 # Функция проверки ввода числа.
 
 def CheckNum(type_date=int, massage="Input number: "):
@@ -24,3 +26,21 @@ try:
     print(count)
 except:
     print("Такого числа нет.")
+
+# ===================================  2  ===========================================
+# Функция проверки ввода числа.
+
+# def CheckNum(type_date=int, massage="Input number: "):
+#     while True:
+#         try:
+#             num = type_date(input(massage))
+#             break
+#         except ValueError:
+#             print('It is not correct number. Try again.')
+#     return num
+
+# list_arr = ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
+# #print(''.join(list(filter(lambda char: char.isdigit(), data))))
+
+# number = CheckNum()
+# print(any(str(number) in s for s in list_arr))
