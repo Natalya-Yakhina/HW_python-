243b21fc0a9c3c7857a332a13e2a5041e2f9523d
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-26 11:02:16 +0300

    проверка числа в строке

--- Seminar_06_Program/Zadacha_01.py
+++ Seminar_06_Program/Zadacha_01.py
@@ -6,26 +6,26 @@
 # ===================================  1 ===========================================
 # Функция проверки ввода числа.
 
-def CheckNum(type_date=int, massage="Input number: "):
-    while True:
-        try:
-            num = type_date(input(massage))
-            break
-        except ValueError:
-            print('It is not correct number. Try again.')
-    return num
+# def CheckNum(type_date=int, massage="Input number: "):
+#     while True:
+#         try:
+#             num = type_date(input(massage))
+#             break
+#         except ValueError:
+#             print('It is not correct number. Try again.')
+#     return num
 
-list_arr = ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
-num = CheckNum()
-# count = 0
-for i in range(len(list_arr)):
-    if str(num) in list_arr[i]:
-        count = i
-        break
-try:
-    print(count)
-except:
-    print("Такого числа нет.")
+# list_arr = ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
+# num = CheckNum()
+# # count = 0
+# for i in range(len(list_arr)):
+#     if str(num) in list_arr[i]:
+#         count = i
+#         break
+# try:
+#     print(count)
+# except:
+#     print("Такого числа нет.")
 
 # ===================================  2  ===========================================
 # Функция проверки ввода числа.
@@ -39,8 +39,8 @@ except:
 #             print('It is not correct number. Try again.')
 #     return num
 
-# list_arr = ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
-# #print(''.join(list(filter(lambda char: char.isdigit(), data))))
+list_arr = ['апап4', 'fdgg3', 'fgdf', '6', 'fg24']
+#print(''.join(list(filter(lambda char: char.isdigit(), data))))
 
-# number = CheckNum()
-# print(any(str(number) in s for s in list_arr))
+number = int(input('Введите число: '))
+print(any(str(number) in s for s in list_arr))
